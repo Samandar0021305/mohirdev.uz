ab99b160e14dd36fe28057c0fc7f6d57b07255d6
Samandar0021305 <samandar021325@gmail.com>
2022-08-16 11:31:54 +0500

    init

--- js/main.js
+++ js/main.js
@@ -4,3 +4,4 @@ var navbarToogleList = document.querySelector(".navbar__toogle__list");
 navbarPic.addEventListener("click",function(){
     navbarToogleList.classList.toggle("navbar__toogle__list_row")
 })
+
